42ddd90f52feee28ebcb6cd0ae537e09863fa46a
gil-son <gilszon@gmail.com>
2023-03-08 14:40:15 -0300

    Variables - example 1

---
+++ terraform-basic/03-variables-with-ec2/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+    type = string
+    description = ""
+    default = "us-east-1"
+}
+
+variable "aws_profile" {
+    type = string
+    description = ""
+    default = "default"
+}
+
+variable "instance_ami" {
+    type = string
+    description = ""
+    default = "ami-006dcf34c09e50022"
+}
+
+variable "instance_type" {
+    type = string
+    description = ""
+    default = "t2.micro"
+}
+
+variable "instance_tags" {
+    type = map(string)
+    description = ""
+    default = {
+        Name = "Ubuntu"
+        Project = "AWS with Terraform"
+    }
+}
