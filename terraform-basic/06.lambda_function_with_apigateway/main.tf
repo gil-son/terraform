fb865649768f9c090460768887b7c52dfcffe5a5
gil-son <gilszon@gmail.com>
2023-03-20 23:14:43 -0300

    Organized resources

--- terraform-basic/06.lambda_function_with_apigateway/main.tf
+++ terraform-basic/06.lambda_function_with_apigateway/main.tf
@@ -1,22 +1,18 @@
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = "AKIA4YJLUZ32SG426CKI"
+  secret_key = "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"
   region     = "us-east-1"
 }
-
-
 
 resource "aws_api_gateway_rest_api" "my_api_gateway" {
   name = "my-api-gateway-name"
 }
-
 
 resource "aws_api_gateway_resource" "my_resource_api_gateway_resource" {
   rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
   parent_id   = aws_api_gateway_rest_api.my_api_gateway.root_resource_id
   path_part   = "mypath"
 }
-
 
 resource "aws_api_gateway_method" "my_api_gateway_method" {
   rest_api_id   = aws_api_gateway_rest_api.my_api_gateway.id
@@ -25,6 +21,17 @@ resource "aws_api_gateway_method" "my_api_gateway_method" {
   authorization = "NONE"
 }
 
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = "the_lambda_function"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  filename      = "lambda_function.zip"
+  role          = aws_iam_role.my_lambda_role.arn
+
+  provisioner "local-exec" {
+    command = "cd lambda && zip -r ../lambda_function.zip ."
+  }
+}
 
 resource "aws_api_gateway_integration" "my_api_gateway_integration" {
   rest_api_id             = aws_api_gateway_rest_api.my_api_gateway.id
@@ -35,60 +42,39 @@ resource "aws_api_gateway_integration" "my_api_gateway_integration" {
   uri                     = aws_lambda_function.my_lambda_function.invoke_arn
 }
 
-
-resource "aws_api_gateway_stage" "prod" {
-  rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
-  deployment_id = aws_api_gateway_deployment.my_api_gateway_deployment.id
-  stage_name = "prod"
-}
-
-resource "aws_lambda_alias" "my_lambda_alias" {
-  name = "prod"
-  function_name = aws_lambda_function.my_lambda_function.function_name
-  function_version = "$LATEST"
-}
-
 resource "aws_api_gateway_deployment" "my_api_gateway_deployment" {
-  depends_on    = [aws_api_gateway_integration.my_api_gateway_integration]
-  rest_api_id   = aws_api_gateway_rest_api.my_api_gateway.id
-
-  stage_description = {
-    description = "Production"
-  }
-
-  stage_name    = aws_api_gateway_stage.prod.stage_name
+  rest_api_id        = aws_api_gateway_rest_api.my_api_gateway.id
+  stage_description = "Production"
   variables = {
     lambdaAlias = aws_lambda_alias.my_lambda_alias.name
   }
 }
 
-
-resource "aws_lambda_function" "my_lambda_function" {
-  function_name = "the_lambda_function"
-  handler = "lambda_function.lambda_handler"
-  runtime = "python3.9"
-  filename = "lambda_function.zip"
-  role = aws_iam_role.my_lambda_role.arn
-
-  provisioner "local-exec" {
-    command = "cd lambda && zip -r ../lambda_function.zip ."
-  }
-}
-
-
 resource "aws_iam_role" "my_lambda_role" {
   name = "lambda_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action   = "sts:AssumeRole"
+        Effect   = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
       }
     ]
   })
+}
+
+resource "aws_lambda_alias" "my_lambda_alias" {
+  name             = "prod"
+  function_name    = aws_lambda_function.my_lambda_function.function_name
+  function_version = "$LATEST"
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api_gateway.id
+  stage_name    = "prod"
+  deployment_id = aws_api_gateway_deployment.my_api_gateway_deployment.id
 }
