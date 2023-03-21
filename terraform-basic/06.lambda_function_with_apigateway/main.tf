72b3cb4701981a173acec680d89d913bea1b647e
gil-son <gilszon@gmail.com>
2023-03-20 21:12:22 -0300

    Organized

--- terraform-basic/06.lambda_function_with_apigateway/main.tf
+++ terraform-basic/06.lambda_function_with_apigateway/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
   region     = "us-east-1"
 }
 
@@ -35,26 +35,60 @@ resource "aws_api_gateway_integration" "my_api_gateway_integration" {
   uri                     = aws_lambda_function.my_lambda_function.invoke_arn
 }
 
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
+  deployment_id = aws_api_gateway_deployment.my_api_gateway_deployment.id
+  stage_name = "prod"
+}
+
+resource "aws_lambda_alias" "my_lambda_alias" {
+  name = "prod"
+  function_name = aws_lambda_function.my_lambda_function.function_name
+  function_version = "$LATEST"
+}
+
 resource "aws_api_gateway_deployment" "my_api_gateway_deployment" {
   depends_on    = [aws_api_gateway_integration.my_api_gateway_integration]
   rest_api_id   = aws_api_gateway_rest_api.my_api_gateway.id
-  stage_name    = "prod"
+
+  stage_description = {
+    description = "Production"
+  }
+
+  stage_name    = aws_api_gateway_stage.prod.stage_name
   variables = {
-    lambdaAlias = aws_lambda_alias.example.name
+    lambdaAlias = aws_lambda_alias.my_lambda_alias.name
   }
 }
 
 
 resource "aws_lambda_function" "my_lambda_function" {
-  function_name = "example_lambda_function"
+  function_name = "the_lambda_function"
   handler = "lambda_function.lambda_handler"
   runtime = "python3.9"
-  filename = "path/to/your/lambda_function.zip"
-  role = aws_iam_role.example.arn
+  filename = "lambda_function.zip"
+  role = aws_iam_role.my_lambda_role.arn
 
-  environment {
-    variables = {
-      ENV_VAR_NAME = "env_var_value"
-    }
+  provisioner "local-exec" {
+    command = "cd lambda && zip -r ../lambda_function.zip ."
   }
+}
+
+
+resource "aws_iam_role" "my_lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
