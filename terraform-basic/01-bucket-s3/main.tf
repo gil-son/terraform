1dd27f4996937671dc3aa03392959277230aa769
gil-son <gilszon@gmail.com>
2023-04-03 12:33:06 -0300

    Default user removed

--- terraform-basic/01-bucket-s3/main.tf
+++ terraform-basic/01-bucket-s3/main.tf
@@ -1,13 +1,11 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  profile = "adm_gil" # If profile is don't declared, the profiel default will be use 
 }
 
 resource "aws_s3_bucket" "my-test-bucket-01070107" {
   bucket = "my-tf-test-bucket-gilszon"
-  acl    = "private"
-  
+    
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
